184c2910497043b5086b93f9e2a19379fbf6f2db
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-15 13:57:41 -0500

    Turtle name

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+	  turtleName:''
   },
   getters: {
+	getTurtleName(state){
+		return this.turtleName;
+	}
   },
   mutations: {
+	setTurtleName(state, name){
+		state.turtleName = name;
+	}
   },
   actions: {
+	setTurtleName(state, name){
+		state.commit("setTurtleName", name);
+	}
   },
   modules: {
   }
